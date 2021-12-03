e6d723619682942d7c9cf13434ebd0ac5594018a
Михаил <mihanik1608@gmail.com>
2021-12-03 15:07:36 +0300

    try4

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ def handle_city(message: types.Message):
     cur.execute(f"SELECT category FROM products WHERE city = '{message.text.strip()}';")
     rows = cur.fetchall()
     for row in rows:
-        item = types.InlineKeyboardButton(''.join(row[0]))
+        item = types.InlineKeyboardButton(text=''.join(row[0]), callback_data=f'{row[0]}')
         markup_inline.add(item)
     bot.send_message(message.chat.id, "Выбери категорию, которая тебя интересует!", reply_markup=markup_inline)
 
